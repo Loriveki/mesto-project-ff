cfa50c4613a0826108422f029f147b3b62fd3a7f
Loriveki <loriveki@mail.ru>
2024-11-16 21:46:22 +0300

    создание и удаление карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,46 @@
-// @todo: Темплейт карточки
+// Находим контейнер для карточек и шаблон карточки
+const cardsContainer = document.querySelector(".places__list");
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".places__item");
 
-// @todo: DOM узлы
+// Функция создания карточки
+function createCard(cardData, deleteCallback) {
+  const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция создания карточки
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-// @todo: Функция удаления карточки
+  // Устанавливаем данные карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  // Добавляем обработчик на иконку удаления
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  if (cardElement && cardElement.parentNode) {
+    cardElement.remove();
+  } else {
+    console.error("Элемент не найден для удаления");
+  }
+}
+
+// Функция для вывода всех карточек
+function renderCards(cards) {
+  cards.forEach((cardData) => {
+    const card = createCard(cardData, deleteCard); 
+    cardsContainer.appendChild(card); 
+  });
+}
+
+// Выводим карточки на страницу
+renderCards(initialCards);
